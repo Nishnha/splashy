561033f922e93d3dc488bfca45b96ed4798c6ae4
= <=>
2017-01-22 13:03:20 -0500

    reupload no merge conflicts

---
+++ Assets/Scripts/GuyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyMovement : MonoBehaviour {
+
+    public float zLimit;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3(rb.position.x, rb.position.y, zLimit);
+        rb.position = pos;
+        
+    }
+}
